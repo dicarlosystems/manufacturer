3a03a8e4ec34cc5e3e46c9c20f52ba1e4c80cbe9
Christopher Di Carlo <chris@dicarlosystems.ca>
2018-05-03 08:27:09 -0400

    Added manufacturer persistence, refactoring

--- Listeners/ProductEventSubscriber.php
+++ Listeners/ProductEventSubscriber.php
@@ -5,25 +5,41 @@ namespace Modules\Manufacturer\Listeners;
 use App\Events\ProductWasCreated;
 use App\Events\ProductWasUpdated;
 use Modules\Manufacturer\Models\ManufacturerProductDetails;
+use Modules\Manufacturer\Repositories\ManufacturerRepository;
 
 class ProductEventSubscriber
 {
-    public function onProductCreated(ProductWasCreated $event)
-    {
-        $product = $event->product;
+    protected $manufacturerRepo;
 
+    public function __construct(ManufacturerRepository $manufacturerRepo) {
+        $this->manufacturerRepo = $manufacturerRepo;
+    }
+
+    protected function fixInput($input) {
         // filter out all non-module fields; convention <module_name>__<field_name>
-        $input = array_filter($event->input, function ($key) {
-            return strpos($key, config('manufacturer.name') . '_') === 0;
+        $input = array_filter($input, function ($key) {
+            return strpos($key, config('manufacturer.name') . '__') === 0;
         }, ARRAY_FILTER_USE_KEY);
 
         // strip the module identifier from the keys
         $keys = array_map(function ($value) {
-            return str_replace(config('manufacturer.name') . '_', '', $value);
+            return str_replace(config('manufacturer.name') . '__', '', $value);
         }, array_keys($input));
 
         // combine the field values with the stripped key names
         $input = array_combine($keys, $input);
+
+        // replace the manufacturer public ID with the internal ID
+        $input['manufacturer_id'] = $this->manufacturerRepo->findByPublicIdsWithTrashed($input['manufacturer_public_id'])->first()->id;
+
+        return $input;
+    }
+
+    public function onProductCreated(ProductWasCreated $event)
+    {
+        $product = $event->product;
+
+        $input = $this->fixInput($event->input);
 
         $details = ManufacturerProductDetails::createNew();
         $details->fill($input);
@@ -36,20 +52,7 @@ class ProductEventSubscriber
         $product = $event->product;
         $details = $product->manufacturerProductDetails()->first();
 
-        // filter out all non-module fields; convention <module_name>__<field_name>
-        $input = array_filter($event->input, function ($key) {
-            return strpos($key, config('manufacturer.name') . '_') === 0;
-        }, ARRAY_FILTER_USE_KEY);
-
-
-
-        // strip the module identifier from the keys
-        $keys = array_map(function ($value) {
-            return str_replace(config('manufacturer.name') . '_', '', $value);
-        }, array_keys($input));
-
-        // combine the field values with the stripped key names
-        $input = array_combine($keys, $input);
+         $input = $this->fixInput($event->input);
 
         if(!$details) {
             $details = ManufacturerProductDetails::createNew();
--- Resources/views/accounts/product.blade.php
+++ Resources/views/accounts/product.blade.php
@@ -1,11 +1,14 @@
+
 @if($product && $product->manufacturerProductDetails()->exists())
-    {{ Former::populateField(config('manufacturer.name') . '_part_number', $product->manufacturerProductDetails()->first()->part_number) }}
-    {{ Former::populateField(config('manufacturer.name') . '_ean13', $product->manufacturerProductDetails()->first()->ean13) }}
-    {{ Former::populateField(config('manufacturer.name') . '_upca', $product->manufacturerProductDetails()->first()->upca) }}
-    {{ Former::populateField(config('manufacturer.name') . '_barcode', $product->manufacturerProductDetails()->first()->barcode) }}
+    {{ Former::populateField(config('manufacturer.name') . '__manufacturer_public_id', $product->manufacturerProductDetails()->first()->manufacturer->public_id) }}
+    {{ Former::populateField(config('manufacturer.name') . '__part_number', $product->manufacturerProductDetails()->first()->part_number) }}
+    {{ Former::populateField(config('manufacturer.name') . '_barcode_ean13', $product->manufacturerProductDetails()->first()->ean13) }}
+    {{ Former::populateField(config('manufacturer.name') . '__upca', $product->manufacturerProductDetails()->first()->upca) }}
+    {{ Former::populateField(config('manufacturer.name') . '__barcode', $product->manufacturerProductDetails()->first()->barcode) }}
 @endif
 
-{!! Former::text(config('manufacturer.name') . '_part_number')->label('Part Number') !!}
-{!! Former::text(config('manufacturer.name') . '_ean13')->label('EAN13') !!}
-{!! Former::text(config('manufacturer.name') . '_upca')->label('UPC-A') !!}
-{!! Former::text(config('manufacturer.name') . '_barcode')->label('Bar Code') !!}
+{!! Former::select(config('manufacturer.name') . '__manufacturer_public_id')->fromQuery($manufacturers, 'name', 'public_id')->placeholder('Select the manufacturer')->label('Manufacturer') !!}
+{!! Former::text(config('manufacturer.name') . '__part_number')->label('Part Number') !!}
+{!! Former::text(config('manufacturer.name') . '__ean13')->label('EAN13') !!}
+{!! Former::text(config('manufacturer.name') . '__upca')->label('UPC-A') !!}
+{!! Former::text(config('manufacturer.name') . '__barcode')->label('Bar Code') !!}
