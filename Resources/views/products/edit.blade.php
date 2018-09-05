2faa82eb096bc1d6b75adc4eebb3acc2688e0576
Christopher Di Carlo <chris@dicarlosystems.ca>
2018-09-04 22:53:25 -0400

    Added serialized to view

--- Resources/views/products/edit.blade.php
+++ Resources/views/products/edit.blade.php
@@ -7,3 +7,4 @@
 {!! Former::select(config('manufacturer.name') . '__manufacturer_public_id')->fromQuery($manufacturers, 'name', 'public_id')->placeholder('Select the manufacturer')->label('Manufacturer') !!}
 {!! Former::text(config('manufacturer.name') . '__part_number')->label('Part Number') !!}
 {!! Former::text(config('manufacturer.name') . '__gtin')->label('GTIN') !!}
+{!! Former::checkbox(config('manufacturer.name') . '__serialized')->label('Is Serialized') !!}
