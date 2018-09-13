acb47ab13812fb2b69a15a3e6ed09af5d9dfc834
Christopher Di Carlo <chris@dicarlosystems.ca>
2018-09-13 00:24:16 -0400

    Fix checkbox value

--- Resources/views/products/edit.blade.php
+++ Resources/views/products/edit.blade.php
@@ -8,4 +8,4 @@
 {!! Former::select(config('manufacturer.name') . '__manufacturer_public_id')->fromQuery($manufacturers, 'name', 'public_id')->placeholder('Select the manufacturer')->label('Manufacturer') !!}
 {!! Former::text(config('manufacturer.name') . '__part_number')->label('Part Number') !!}
 {!! Former::text(config('manufacturer.name') . '__gtin')->label('GTIN') !!}
-{!! Former::checkbox(config('manufacturer.name') . '__serialized')->label('Is Serialized') !!}
+{!! Former::checkbox(config('manufacturer.name') . '__serialized')->label('Is Serialized')->value(1) !!}
